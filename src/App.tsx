5bea2d6fcc639b7d8f04f316b192141c8f14d9ff
Vasya <rusnakvasa3@gmail.com>
2025-12-17 22:55:50 +0200

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React from 'react';
 type State = {
   pressedKey: string;
-}
+};
 export class App extends React.Component<{}, State> {
   state: State = {
     pressedKey: '',
   };
+
   handleKeyUp = (event: KeyboardEvent) => {
     this.setState({
       pressedKey: event.key,
     });
   };
+
   componentDidMount() {
-    document.addEventListener("keyup", this.handleKeyUp);
+    document.addEventListener('keyup', this.handleKeyUp);
   }
-componentWillUnmount() {
-    document.removeEventListener("keyup", this.handleKeyUp);
-}
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
   render() {
     const { pressedKey } = this.state;
 
     return (
       <div className="App">
         <p className="App__message">
-          {pressedKey===''? 'Nothing was pressed yet':`The last pressed key is [${pressedKey}]`}
+          {pressedKey === ''
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${pressedKey}]`}
         </p>
       </div>
     );
